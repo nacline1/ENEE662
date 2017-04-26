a185945b4183b85bdab59c7c244b0e510d7d3510
nacline1 <nacline1@umbc.edu>
2017-04-26 15:30:49 -0400

    Mathematical analysis and results from 100x10 run

--- TrafficRouteSim.m
+++ TrafficRouteSim.m
@@ -58,10 +58,10 @@ if isempty(team_data)==1
     team_data='Supporting_Data_Team_04.mat';
 end
 if isempty(num_trips)==1
-    num_trips=1; %REPLACE with 10
+    num_trips=10; %REPLACE with 10
 end
 if isempty(num_scenarios)==1
-    num_scenarios=1;  %REPLACE with TBD
+    num_scenarios=10;  %REPLACE with TBD
 end
 
 
@@ -115,3 +115,43 @@ while trip<=num_trips
 end
 fprintf('%f%% [%f %f %f]\n',mean(savings_perc),SF.p);
 
+%%%%%%%%%%%%%%%%%%%%%%%%% Data Analysis %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+hold on;
+% Plot un-sorted percent savings
+title('Percent Time Savings Per Iteration');
+xlabel('Iteration');
+ylabel('Percent Time Saved');
+plot(savings_perc);
+
+% Calculate and plot sorted percent savings
+sorted_savings = sort(savings_perc);
+plot(sorted_savings);
+legend('Unsorted', 'Sorted');
+
+% Bin data and display on histogram
+figure;
+hold on;
+% Calculate number of histogram bins via Sturge's rule
+nBins = floor(1 + log(size(sorted_savings, 2))/log(2));
+% Normalize Histogram
+histogram(sorted_savings, nBins, 'Normalization', 'pdf');
+% Fit exponential to sorted data (dfittool analysis to determine best fit)
+savings_fit = fitdist(sorted_savings', 'Exponential');
+y = pdf(savings_fit, sorted_savings');
+% Display fit
+plot(sorted_savings, y);
+xlabel('Percent Saved');
+ylabel('Probability Density');
+legend('Binned Trip Time Data', 'Exponential Fit');
+
+% Get 95% confidence interval
+savings_ci = paramci(savings_fit, 'Alpha', 0.05);
+fprintf('95%% Confidence Interval [%.4f, %.4f]\n', savings_ci(1), savings_ci(2));
+% Check if lower CI bound is > 5% threshold to answer question
+if(savings_ci(1) > 5)
+   fprintf('Predictive routing DOES save more than 5%% of the trip time to a 95%% confidence interval\n');
+else
+   fprintf('Predictive routing DOES NOT save more than 5%% of the trip time to a 95%% confidence interval\n');
+end
+
+fprintf('Percent Re-Routes %.2f\n', sum(sum(Results.ReRouted)) / (num_trips * num_scenarios) * 100);
